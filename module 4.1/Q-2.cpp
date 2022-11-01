8ae084485837fd06e94cb0ff6cbc0a45877a5145
Abhimauryaa <110964150+Abhimauryaa@users.noreply.github.com>
2022-11-01 19:28:33 +0530

    Add files via upload

---
+++ module 4.1/Q-2.cpp
@@ -0,0 +1,16 @@
+/*
+
+
+Q-2 what is OOP? List OOP concepts?
+	--> OOP stands for Object Oriented programming
+	--> Procedural programming is about writing procedures or function that perform operations on the data while object-oriented programming is about creating objects that contain both data and functions.
+
+	-->List of OOP concept:
+		
+		1.	Class
+		2.	Object
+		3.	Abstraction
+		4.	Inheritance
+		5.	Polymorphism
+		6.	Encapsulation
+
---
+++ module 4.1/Q-3.cpp
@@ -0,0 +1,19 @@
+/*
+
+	
+	
+	Q-3 What is the difference between OOP and POP? 
+
+	--> OOP : object oriented programming.
+			- OOP follows bottom-up approach.
+			- A program is divided into objects and their interactions.
+			- inheritance is supported.
+			- Ex: c++, java.
+			
+	--> POP : procedures oriented programming.
+	 		- POP follows top-down approach.
+	 		- A program is divided into function and they interact.
+			- inheritance is not supported.
+			- c,pascal.
+
+
